dbb381b180adc432e53fdcd9d9a7041dcc7f3c89
Jun Choi <cjh4549@gmail.com>
2021-11-30 18:19:00 -0500

    moved the tasks state to app for global state management

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Doctor's appointment",
+        day: 'June 2nd at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Grocery Shopping",
+        day: 'June 5th at 1:00pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Walking my dog",
+        day: 'June 10th at 9:00am',
+        reminder: false,
+    }
+  ])
+
   return (
     <div className="App">
       <Header title="Task Tracker"/>
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,13 @@
-const tasks = [
-    {
-        id: 1,
-        text: "Doctor's appointment",
-        day: 'June 2nd at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Grocery Shopping",
-        day: 'June 5th at 1:00pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Walking my dog",
-        day: 'June 10th at 9:00am',
-        reminder: false,
-    }
-]
 
-const Tasks = () => {
+
+const Tasks = ({ tasks }) => {
+
     return(
-        
+        <>
+            {tasks.map((task) => (
+                <h3 key={task.id}>{task.text}</h3>
+            ))}
+        </>
     )
 }
 
