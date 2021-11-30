900ac67a36b029d2f4db83d627c7aa80332857d8
Jun Choi <cjh4549@gmail.com>
2021-11-30 18:06:44 -0500

    added tasks components with hard coded data objects

---
+++ src/components/Tasks.js
@@ -0,0 +1,28 @@
+const tasks = [
+    {
+        id: 1,
+        text: "Doctor's appointment",
+        day: 'June 2nd at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Grocery Shopping",
+        day: 'June 5th at 1:00pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Walking my dog",
+        day: 'June 10th at 9:00am',
+        reminder: false,
+    }
+]
+
+const Tasks = () => {
+    return(
+        
+    )
+}
+
+export default Tasks
