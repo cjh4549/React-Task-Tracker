67d7dbd42f287718296b699deea03d79ff80fe1d
Jun Choi <cjh4549@gmail.com>
2021-11-30 19:01:57 -0500

    added delete icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
+import reactDom from 'react-dom'
+import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task }) => {
-    console.log(task)
     return(
         <div className='task'>
-            <h3>{task.text}</h3>
+            <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} /></h3>
             <p>{task.day}</p>
         </div>
     )
